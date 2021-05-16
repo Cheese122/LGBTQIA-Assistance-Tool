f6272f65aaec8a05edcfec65fb08150d8c111ad8
Aidan Finley <yalnifnadia@gmail.com>
2021-05-16 19:01:57 +1000

    Apsec added

---
+++ LGBTQIA/MainFormRoot.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainFormRoot
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblOutput = New System.Windows.Forms.Label()
+        Me.cbxAspec = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblOutput
+        '
+        Me.lblOutput.AutoSize = True
+        Me.lblOutput.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.lblOutput.Font = New System.Drawing.Font("Verdana", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lblOutput.Location = New System.Drawing.Point(571, 280)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(189, 45)
+        Me.lblOutput.TabIndex = 0
+        Me.lblOutput.Text = "You are: "
+        Me.lblOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'cbxAspec
+        '
+        Me.cbxAspec.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbxAspec.FormattingEnabled = True
+        Me.cbxAspec.Items.AddRange(New Object() {"Allosexual (Not Aspec)", "Demisexual", "Gray Ace", "Asexual", "Other Aspec"})
+        Me.cbxAspec.Location = New System.Drawing.Point(66, 119)
+        Me.cbxAspec.Name = "cbxAspec"
+        Me.cbxAspec.Size = New System.Drawing.Size(207, 22)
+        Me.cbxAspec.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(55, 98)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(231, 18)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Sexual Attraction Spectrum"
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"Alloromantic (Not Aromantic)", "Demiromantic", "Gray Romantic", "Aromantic"})
+        Me.ComboBox2.Location = New System.Drawing.Point(66, 280)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(207, 22)
+        Me.ComboBox2.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(55, 259)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(253, 18)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Romantic Attraction Spectrum"
+        '
+        'MainFormRoot
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 14.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(196, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(1184, 661)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cbxAspec)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.MinimumSize = New System.Drawing.Size(1200, 700)
+        Me.Name = "MainFormRoot"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "LGBTQIA+ Assistance Tool"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblOutput As Label
+    Friend WithEvents cbxAspec As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Label2 As Label
+End Class
